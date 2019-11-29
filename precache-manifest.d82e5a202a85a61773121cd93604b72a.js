3cf943e09b89495530f98f6f1f3d27670b99bcf2
Leo Kamwathi <Leo.Kamwathi@gmail.com>
2019-11-29 17:03:48 +0300

    Updates

---
+++ precache-manifest.d82e5a202a85a61773121cd93604b72a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedcfdd68e1a6b0255111d1632545287",
+    "url": "/react_gh_page_test/index.html"
+  },
+  {
+    "revision": "b186293a5f735c5483c0",
+    "url": "/react_gh_page_test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f82f0404368e8c54d5db",
+    "url": "/react_gh_page_test/static/js/2.023cda59.chunk.js"
+  },
+  {
+    "revision": "b186293a5f735c5483c0",
+    "url": "/react_gh_page_test/static/js/main.cd62b6b1.chunk.js"
+  },
+  {
+    "revision": "f45a2d9f90413168b20a",
+    "url": "/react_gh_page_test/static/js/runtime-main.0b5e791c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react_gh_page_test/static/media/logo.25bf045c.svg"
+  }
+]);
